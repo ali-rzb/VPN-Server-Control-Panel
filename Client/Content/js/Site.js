51ba8b83108fc80cf466f05a6694a92d5d50f924
ali-rzb <itroozbehi@gmail.com>
2023-11-15 10:10:48 +0330

    .gitignore is now working

--- Client/Content/js/Site.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function() {
-    var $j_object = $(".myTable");
-    $j_object.each(function () {
-        var dataCount = $(this).children(".row:not(.myHeader):not(.myFooter)").length;
-        if (dataCount === 0) {
-            $(this).children().css("display","none");
-        }
-    });
-})
